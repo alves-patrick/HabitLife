3512baa0cf94c112b883f217ee8e988cba86feb1
Patrick Alves <patrickalvesarts@gmail.com>
2024-03-05 20:43:29 -0300

    Estrutura do projeto SwiftUI + Splash Case

---
+++ HabitLife/Splash/Model/SplashUiState.swift
@@ -0,0 +1,8 @@
+//
+//  SplashUiState.swift
+//  HabitLife
+//
+//  Created by Patrick Alves on 05/03/24.
+//
+
+import Foundation
---
+++ HabitLife/Splash/View/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  HabitLife
+//
+//  Created by Patrick Alves on 05/03/24.
+//
+
+import Foundation

f4088a7729c17ad4dc312ba9729b717055c48212
Patrick Alves <patrickalvesarts@gmail.com>
2024-03-05 20:16:10 -0300

    Initial Commit

---
+++ HabitLife/HabitLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitLifeApp.swift
+//  HabitLife
+//
+//  Created by Patrick Alves on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
